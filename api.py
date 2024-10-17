a34cb36515d4b7f9d6865a8658806514db05e8bb
YuriSouzaS <yuriss363@gmail.com>
2024-10-17 16:31:01 -0300

    init

---
+++ api.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+
+def getAllProduto():
+    res = requests.get("https://wiuhi.pythonanywhere.com/")    
+    date = res.json()
+    return date  
+
+
+def save(name: str, amount: int, desc: str, image: str, price: float):
+    url = "https://wiuhi.pythonanywhere.com/produtos/add"    
+    dados = {'amount': amount, 'desc': desc, 'image': image, 'name': name, 'price': price}
+    data_json = json.dumps(dados)
+    headers={"Content-Type": "application/json"}
+    response = requests.post(url, data=data_json, headers=headers)
+    
+    if response.status_code == 200:
+        print("sucesso!")
+    else:
+        print("Erro", response.text)
+
---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, url_for, request, redirect
+from api import getAllProduto, save
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    date = getAllProduto()
+    return render_template('index.html', date=date)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        name: str = request.form['nameProduto']
+        desc: str = request.form['desc']
+        image: str = request.form['image']
+        amount: int = request.form['quantidade']
+        price: float = request.form['price']
+        
+        print(name, desc, image, amount, price)
+        
+        save(name, amount, desc,image, price)
+        return redirect(url_for('index'))
+         
+    return render_template('form.html')
+
+
+if __name__ == "__main__":
+    app.run()
